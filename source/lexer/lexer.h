199f859d08e988fd90a214902b2a270d1f841fb7
Salah <salah.bouheniche@gmail.com>
2024-01-09 01:10:07 +0100

    Add lexer dir

---
+++ source/lexer/lexer.h
@@ -0,0 +1,19 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   lexer.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: sbouheni <sbouheni@student.42mulhouse.fr>  +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/01/09 01:04:46 by sbouheni          #+#    #+#             */
+/*   Updated: 2024/01/09 01:07:41 by sbouheni         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef LEXER_H
+# define LEXER_H
+
+// Open file and return file descriptor
+int	open_file(char *file_name);
+
+#endif
