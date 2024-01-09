d79e301ec11c2b1d44028615e8db145a5bc4cb12
Salah <salah.bouheniche@gmail.com>
2024-01-09 01:09:30 +0100

    Add comment

--- source/error/error.h
+++ source/error/error.h
@@ -6,13 +6,14 @@
 /*   By: sbouheni <sbouheni@student.42mulhouse.fr>  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/27 04:49:46 by sbouheni          #+#    #+#             */
-/*   Updated: 2024/01/08 11:06:43 by sbouheni         ###   ########.fr       */
+/*   Updated: 2024/01/09 01:09:02 by sbouheni         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef ERROR_H
 # define ERROR_H
 
+// Print error_msg to std_err and exit the program
 int	print_exit_error(char *error_msg);
 
 #endif
