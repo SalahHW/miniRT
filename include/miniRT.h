b15c0f62a0ab3a5ea66a4f9d8b29a38b0c141acf
salah <salah.bouheniche@gmail.com>
2024-01-20 03:44:55 +0100

    Update miniRT.h include paths

--- include/miniRT.h
+++ include/miniRT.h
@@ -6,17 +6,20 @@
 /*   By: sbouheni <sbouheni@student.42mulhouse.fr>  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/27 04:34:16 by sbouheni          #+#    #+#             */
-/*   Updated: 2024/01/09 00:43:55 by sbouheni         ###   ########.fr       */
+/*   Updated: 2024/01/18 09:00:39 by sbouheni         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef MINIRT_H
 # define MINIRT_H
 
+# include "../source/error/error.h"
+# include "../source/lexer/lexer.h"
+# include "../source/lexer/element.h"
+# include "../source/parser/parser.h"
+
 # include "./libft/libft.h"
 # include "./minilibx-linux/mlx.h"
-# include "../source/error/error.h"
-# include "../source/parser/parser.h"
-# include "window.h"
+# include <fcntl.h>
 
 #endif
