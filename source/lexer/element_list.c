c5354ed735509d35ba7e1214c83a0e73becf628b
salah <salah.bouheniche@gmail.com>
2024-01-20 03:34:57 +0100

    Add element list handler

--- source/lexer/element_list.c
+++ source/lexer/element_list.c
@@ -1,3 +1,28 @@
-//
-// Created by salah on 1/18/24.
-//
+#include "../../include/miniRT.h"
+
+t_element_list	*init_element_list(void)
+{
+	t_element_list	*element_list;
+
+	element_list = malloc(sizeof(t_element_list));
+	element_list->head = NULL;
+	element_list->tail = NULL;
+	return (element_list);
+}
+
+void	add_element(t_element_list *list, t_element *new_element)
+{
+	if (!new_element)
+		return ;
+	new_element->next = NULL;
+	if (!list->head)
+	{
+		list->head = new_element;
+		list->tail = new_element;
+	}
+	else
+	{
+		list->tail->next = new_element;
+		list->tail = new_element;
+	}
+}
