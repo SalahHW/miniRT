b7594522bd5f082336dd47143a7dc64457275dd0
Salah <salah.bouheniche@gmail.com>
2024-01-24 02:08:03 +0100

    Refactor process_ambient_light function to handle intensity validation

--- source/element/ambient_light.c
+++ source/element/ambient_light.c
@@ -6,12 +6,19 @@
 /*   By: sbouheni <sbouheni@student.42mulhouse.fr>  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/21 08:29:33 by sbouheni          #+#    #+#             */
-/*   Updated: 2024/01/22 02:14:28 by sbouheni         ###   ########.fr       */
+/*   Updated: 2024/01/23 03:03:29 by sbouheni         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../include/miniRT.h"
 
-void	process_ambient_light(char *line, t_element *element)
+void	process_ambient_light(char **data, t_element *element)
 {
+	t_ambient_light	ambient_light;
+
+	if (*data[1] == '0' || *data[1] == '1')
+		ambient_light.intensity = ft_atof(data[1]);
+	if (ambient_light.intensity < 0 || ambient_light.intensity > 1)
+		print_exit_error("Ambient light intensity must be between 0 and 1");
+	element->u_element.ambient_light = ambient_light;
 }
