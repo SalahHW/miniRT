8cef0fdaeaae81ba937277e10ded09d017ba7aea
Salah <salah.bouheniche@gmail.com>
2024-01-27 06:33:56 +0100

    Refactor lexer function to use context struct

--- source/lexer/lexer.c
+++ source/lexer/lexer.c
@@ -3,27 +3,20 @@
 /*                                                        :::      ::::::::   */
 /*   lexer.c                                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: sbouheni <sbouheni@student.42mulhouse.fr>  +#+  +:+       +#+        */
+/*   By: sbouheni <sbouheni@student.42mulhouse.f    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/09 15:43:37 by sbouheni          #+#    #+#             */
-/*   Updated: 2024/01/22 02:22:01 by sbouheni         ###   ########.fr       */
+/*   Updated: 2024/01/27 03:51:36 by sbouheni         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../include/miniRT.h"
 
-void	run_lexer(char *file_path)
+int	run_lexer(t_context *context)
 {
-	t_rt_file		rt_file;
-	t_element_list	*element_list;
-
-	rt_file.file_name = file_path;
-	open_rt_file(&rt_file);
-	element_list = init_element_list();
-	if (!element_list)
-	{
-		//close_rt_file(&rt_file);
-		print_exit_error("Unable to allocate memory for element list");
-	}
-	extract_rt_file_data(&rt_file, element_list);
+	if (open_rt_file(context->rt_file) == FAILURE)
+		return (FAILURE);
+	if (extract_rt_file_data(context) == FAILURE)
+		return (FAILURE);
+	return (SUCCESS);
 }
