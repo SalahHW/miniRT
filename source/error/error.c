07f5a46d8a2996c185461735c1a45891551b8aa1
Salah <salah.bouheniche@gmail.com>
2024-01-27 05:41:24 +0100

    Refactor error handling functions

--- source/error/error.c
+++ source/error/error.c
@@ -3,19 +3,24 @@
 /*                                                        :::      ::::::::   */
 /*   error.c                                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: sbouheni <sbouheni@student.42mulhouse.fr>  +#+  +:+       +#+        */
+/*   By: sbouheni <sbouheni@student.42mulhouse.f    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/27 04:43:28 by sbouheni          #+#    #+#             */
-/*   Updated: 2024/01/22 02:16:10 by sbouheni         ###   ########.fr       */
+/*   Updated: 2024/01/27 03:58:36 by sbouheni         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../include/miniRT.h"
 
-int	print_exit_error(char *message)
+void print_error(char *message)
 {
 	ft_putstr_fd("Error\n", 2);
 	ft_putstr_fd(message, 2);
 	ft_putstr_fd("\n", 2);
+}
+
+void print_exit_error(char *message)
+{
+	print_error(message);
 	exit(EXIT_FAILURE);
 }
--- source/error/error.h
+++ source/error/error.h
@@ -3,17 +3,19 @@
 /*                                                        :::      ::::::::   */
 /*   error.h                                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: sbouheni <sbouheni@student.42mulhouse.fr>  +#+  +:+       +#+        */
+/*   By: sbouheni <sbouheni@student.42mulhouse.f    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/27 04:49:46 by sbouheni          #+#    #+#             */
-/*   Updated: 2024/01/09 01:09:02 by sbouheni         ###   ########.fr       */
+/*   Updated: 2024/01/27 03:59:27 by sbouheni         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef ERROR_H
 # define ERROR_H
 
+// Print error_msg to std_err
+void    print_error(char *error_msg);
 // Print error_msg to std_err and exit the program
-int	print_exit_error(char *error_msg);
+void    print_exit_error(char *error_msg);
 
 #endif
