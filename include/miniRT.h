8dd796fb085669df4c000d9dfc62a624fbcf29f0
Salah <salah.bouheniche@gmail.com>
2024-01-27 03:15:03 +0100

    Update miniRT.h with new struct t_context

--- include/miniRT.h
+++ include/miniRT.h
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   miniRT.h                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: sbouheni <sbouheni@student.42mulhouse.fr>  +#+  +:+       +#+        */
+/*   By: sbouheni <sbouheni@student.42mulhouse.f    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/27 04:34:16 by sbouheni          #+#    #+#             */
-/*   Updated: 2024/01/23 02:47:00 by sbouheni         ###   ########.fr       */
+/*   Updated: 2024/01/27 03:14:38 by sbouheni         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,5 +21,11 @@
 # include "./libft/libft.h"
 # include "./minilibx-linux/mlx.h"
 # include <fcntl.h>
+
+typedef struct s_context
+{
+	t_rt_file		*rt_file;
+	t_element_list	*element_list;
+}					t_context;
 
 #endif
