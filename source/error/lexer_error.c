450a31b85d9aec5cc00b2e12bac6a4ad5dc1350b
Salah <salah.bouheniche@gmail.com>
2024-01-28 05:15:59 +0100

    Add lexer_error function to handle lexer errors

--- source/error/lexer_error.c
+++ source/error/lexer_error.c
@@ -1,23 +1,18 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   error.h                                            :+:      :+:    :+:   */
+/*   lexer_error.c                                      :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: sbouheni <sbouheni@student.42mulhouse.f    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/12/27 04:49:46 by sbouheni          #+#    #+#             */
-/*   Updated: 2024/01/27 07:23:43 by sbouheni         ###   ########.fr       */
+/*   Created: 2024/01/27 07:01:39 by sbouheni          #+#    #+#             */
+/*   Updated: 2024/01/27 07:02:44 by sbouheni         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef ERROR_H
-# define ERROR_H
+#include "../../include/miniRT.h"
 
-// Print error_msg to std_err
-void    print_error(char *error_msg);
-// Print error_msg to std_err and exit the program
-void    print_exit_error(char *error_msg);
-
-void    lexer_error(t_context *context);
-
-#endif
+void    lexer_error(t_context *context)
+{
+    clear_context(context);
+}
