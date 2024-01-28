070d61d2b22b853313e67ff26caafbe8550aea6f
Salah <salah.bouheniche@gmail.com>
2024-01-28 06:35:20 +0100

    Update ambient_light.c file

--- source/element/ambient_light.c
+++ source/element/ambient_light.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   ambient_light.c                                    :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: sbouheni <sbouheni@student.42mulhouse.fr>  +#+  +:+       +#+        */
+/*   By: sbouheni <sbouheni@student.42mulhouse.f    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/21 08:29:33 by sbouheni          #+#    #+#             */
-/*   Updated: 2024/01/23 03:03:29 by sbouheni         ###   ########.fr       */
+/*   Updated: 2024/01/28 06:31:10 by sbouheni         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,11 +14,11 @@
 
 void	process_ambient_light(char **data, t_element *element)
 {
-	t_ambient_light	ambient_light;
+}
 
-	if (*data[1] == '0' || *data[1] == '1')
-		ambient_light.intensity = ft_atof(data[1]);
-	if (ambient_light.intensity < 0 || ambient_light.intensity > 1)
-		print_exit_error("Ambient light intensity must be between 0 and 1");
-	element->u_element.ambient_light = ambient_light;
+void clear_ambient_light(t_ambient_light *ambient_light)
+{
+	clear_decimal(ambient_light->intensity);
+	clear_color(ambient_light->color);
+	free(ambient_light);
 }
