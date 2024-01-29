f0cdf74df46a4000aaffe51e64b4b23b5e93603b
Salah <salah.bouheniche@gmail.com>
2024-01-29 06:59:42 +0100

    Update lexer.h file

--- source/lexer/lexer.h
+++ source/lexer/lexer.h
@@ -6,7 +6,7 @@
 /*   By: sbouheni <sbouheni@student.42mulhouse.f    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/09 01:04:46 by sbouheni          #+#    #+#             */
-/*   Updated: 2024/01/27 07:00:15 by sbouheni         ###   ########.fr       */
+/*   Updated: 2024/01/28 07:41:51 by sbouheni         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,7 +27,9 @@ typedef struct s_rt_file
 // Execute lexer process
 int								run_lexer(t_context *context);
 // Open file and return file descriptor
-int								open_rt_file(t_rt_file *rt_file);
+t_rt_file						*open_rt_file(char *file_path);
+// Close file and free memory
+void							clear_file(t_rt_file *rt_file);
 // Read file and fill element_list
 int								extract_rt_file_data(t_context *context);
 // Check if line is empty
