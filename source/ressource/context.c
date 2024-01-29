486dd1fe53f78195b05bb80083db49d9fa3f14d0
Salah <salah.bouheniche@gmail.com>
2024-01-29 06:59:52 +0100

    Refactor init_context function to accept rt_file_path parameter

--- source/ressource/context.c
+++ source/ressource/context.c
@@ -6,24 +6,24 @@
 /*   By: sbouheni <sbouheni@student.42mulhouse.f    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/27 03:19:18 by sbouheni          #+#    #+#             */
-/*   Updated: 2024/01/28 05:08:45 by sbouheni         ###   ########.fr       */
+/*   Updated: 2024/01/29 06:17:07 by sbouheni         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../include/miniRT.h"
 
-t_context    *init_context(void)
+t_context    *init_context(char *rt_file_path)
 {
     t_context	*context;
 
     context = malloc(sizeof(t_context));
     if (!context)
         print_exit_error("Unable to allocate memory for context");
-    context->rt_file = malloc(sizeof(t_rt_file));
+    context->rt_file = open_rt_file(rt_file_path);
     if (!context->rt_file)
     {
         free(context);
-        print_exit_error("Unable to allocate memory for rt file");
+        exit(EXIT_FAILURE);
     }
     context->element_list = init_element_list();
     if (!context->element_list)
@@ -37,7 +37,7 @@ t_context    *init_context(void)
 void   clear_context(t_context *context)
 {
     if (context->rt_file)
-        free(context->rt_file);
+        clear_file(context->rt_file);
     if (context->element_list)
         clear_element_list(context->element_list);
     free(context);
