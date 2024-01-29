5dba9f8771194098d417533040d94038dcf31cfb
Salah <salah.bouheniche@gmail.com>
2024-01-29 06:58:08 +0100

    Refactor element clearing logic and add initialization check

--- source/element/element.c
+++ source/element/element.c
@@ -6,7 +6,7 @@
 /*   By: sbouheni <sbouheni@student.42mulhouse.f    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/22 02:14:10 by sbouheni          #+#    #+#             */
-/*   Updated: 2024/01/28 06:24:09 by sbouheni         ###   ########.fr       */
+/*   Updated: 2024/01/29 06:54:16 by sbouheni         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,22 +20,27 @@ t_element	*init_element(void)
 	if (!new_element)
 		return (NULL);
 	new_element->identifier = unknown;
+	new_element->initialized = 0;
 	new_element->next = NULL;
 	return (new_element);
 }
 
 void clear_element(t_element *element)
 {
-	if (element->identifier == ambient_light)
-		clear_ambient_light(element);
-	else if (element->identifier == camera)
-		clear_camera(element);
-	else if (element->identifier == light)
-		clear_light(element);
-	else if (element->identifier == sphere)
-		clear_sphere(element);
-	else if (element->identifier == plane)
-		clear_plane(element);
-	else if (element->identifier == cylinder)
-		clear_cylinder(element);
+	if (element->initialized)
+	{
+		if (element->identifier == ambient_light)
+			clear_ambient_light(element);
+		else if (element->identifier == camera)
+			clear_camera(element);
+		else if (element->identifier == light)
+			clear_light(element);
+		else if (element->identifier == sphere)
+			clear_sphere(element);
+		else if (element->identifier == plane)
+			clear_plane(element);
+		else if (element->identifier == cylinder)
+			clear_cylinder(element);
+	}
+	free(element);
 }
