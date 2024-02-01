c596450e230133f72dae3ed40f2a795bc0959a93
Salah <salah.bouheniche@gmail.com>
2024-02-01 04:56:05 +0100

    Update color.c file with new_color and extract_color functions

--- source/element/color.c
+++ source/element/color.c
@@ -6,7 +6,7 @@
 /*   By: sbouheni <sbouheni@student.42mulhouse.f    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/28 06:43:55 by sbouheni          #+#    #+#             */
-/*   Updated: 2024/01/28 06:45:09 by sbouheni         ###   ########.fr       */
+/*   Updated: 2024/02/01 04:45:17 by sbouheni         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,7 +25,26 @@ t_color *new_color(int red, int green, int blue)
     return (color);
 }
 
+t_color *extract_color(char *str)
+{
+    t_color *color;
+    char **split;
+    
+    split = ft_split(str, ',');
+    if (!split || split_length(split) != 3)
+        return (NULL);
+    if (!is_int_compatible(split[0]) || !is_int_compatible(split[1]) || !is_int_compatible(split[2]))
+    {
+        p_free_splited_str(split);
+        return (NULL);
+    }
+    color = new_color(ft_atoi(split[0]), ft_atoi(split[1]), ft_atoi(split[2]));
+    p_free_splited_str(split);
+    return (color);
+}
+
 void clear_color(t_color *color)
 {
-    free(color);
+    if (color)
+        free(color);
 }
