dcccdb1601cbddb6a81a88452f29ab3454700706
Salah <salah.bouheniche@gmail.com>
2024-02-01 04:55:53 +0100

    Update ambient light processing and clearing functions

--- source/element/ambient_light.c
+++ source/element/ambient_light.c
@@ -6,7 +6,7 @@
 /*   By: sbouheni <sbouheni@student.42mulhouse.f    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/21 08:29:33 by sbouheni          #+#    #+#             */
-/*   Updated: 2024/01/28 06:31:10 by sbouheni         ###   ########.fr       */
+/*   Updated: 2024/02/01 04:53:03 by sbouheni         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,11 +14,15 @@
 
 void	process_ambient_light(char **data, t_element *element)
 {
+	t_ambient_light ambient_light;
+
+	ambient_light.intensity = extract_decimal(data[1]);
+	ambient_light.color = extract_color(data[2]);
+	element->u_element.ambient_light = ambient_light;
 }
 
-void clear_ambient_light(t_ambient_light *ambient_light)
+void clear_ambient_light(t_element *element)
 {
-	clear_decimal(ambient_light->intensity);
-	clear_color(ambient_light->color);
-	free(ambient_light);
+	clear_decimal(element->u_element.ambient_light.intensity);
+	clear_color(element->u_element.ambient_light.color);
 }
