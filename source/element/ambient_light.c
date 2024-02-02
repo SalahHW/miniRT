99047a0fa95d1e20c47a29c3b01c7da9a1dbdf04
Salah <salah.bouheniche@gmail.com>
2024-02-02 18:37:37 +0100

    Update ambient light initialization and error handling

--- source/element/ambient_light.c
+++ source/element/ambient_light.c
@@ -6,7 +6,7 @@
 /*   By: sbouheni <sbouheni@student.42mulhouse.f    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/21 08:29:33 by sbouheni          #+#    #+#             */
-/*   Updated: 2024/02/01 04:53:03 by sbouheni         ###   ########.fr       */
+/*   Updated: 2024/02/02 12:41:29 by sbouheni         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,13 +16,18 @@ void	process_ambient_light(char **data, t_element *element)
 {
 	t_ambient_light ambient_light;
 
+	if (split_length(data) != 3)
+		return ;
 	ambient_light.intensity = extract_decimal(data[1]);
 	ambient_light.color = extract_color(data[2]);
 	element->u_element.ambient_light = ambient_light;
+	element->initialized = 1;
 }
 
 void clear_ambient_light(t_element *element)
 {
+	if (!element->initialized)
+		return ;
 	clear_decimal(element->u_element.ambient_light.intensity);
 	clear_color(element->u_element.ambient_light.color);
 }
