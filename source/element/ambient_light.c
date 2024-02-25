3846b53b282b19e321d04cc6c23614a977cb6a3d
Salah <salah.bouheniche@gmail.com>
2024-02-25 19:08:07 +0100

    Refactor process_ambient_light function to improve code readability and error handling

--- source/element/ambient_light.c
+++ source/element/ambient_light.c
@@ -6,11 +6,20 @@
 /*   By: sbouheni <sbouheni@student.42mulhouse.f    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/21 08:29:33 by sbouheni          #+#    #+#             */
-/*   Updated: 2024/02/09 19:40:27 by sbouheni         ###   ########.fr       */
+/*   Updated: 2024/02/23 10:44:01 by sbouheni         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../include/miniRT.h"
+
+static int check_ambient_light_values(char **data)
+{
+	if (!is_double_compatible(data[1]))
+		return (print_error("Unable to get ambient light intensity"));
+	if (is_color_compatible(data[2]) == FAILURE)
+		return (print_error("Unable to get ambient light color"));
+	return (SUCCESS);
+}
 
 int	process_ambient_light(char **data, t_element *element)
 {
@@ -18,14 +27,13 @@ int	process_ambient_light(char **data, t_element *element)
 
 	if (split_length(data) != 3)
 		return(print_error("Invalid values for ambient light"));
-	if (!is_double_compatible(data[1]))
-		return (print_error("Unable to get ambient light intensity"));
+	if (!check_ambient_light_values(data))
+		return (FAILURE);
 	ambient_light.intensity = string_to_double(data[1]);
 	ambient_light.color = extract_color(data[2]);
-	if (!ambient_light.color)
-		return (print_error("Unable to get ambient light color"));
 	element->u_element.ambient_light = ambient_light;
 	element->initialized = 1;
+	return (SUCCESS);
 }
 
 void clear_ambient_light(t_element *element)
