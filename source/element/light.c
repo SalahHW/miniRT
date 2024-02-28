214d1a02065b760e36a3602591e7de0f1a4a2a19
Salah <salah.bouheniche@gmail.com>
2024-02-28 04:54:31 +0100

    Refactor light validation in process_light function

--- source/element/light.c
+++ source/element/light.c
@@ -6,18 +6,31 @@
 /*   By: sbouheni <sbouheni@student.42mulhouse.f    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/21 08:30:58 by sbouheni          #+#    #+#             */
-/*   Updated: 2024/02/09 19:43:16 by sbouheni         ###   ########.fr       */
+/*   Updated: 2024/02/28 04:50:03 by sbouheni         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../include/miniRT.h"
 
+static int check_light_values(char **data)
+{
+    if (split_length(data) != 4)
+        return (print_error("Invalid syntax for light"));
+    if (is_vector_compatible(data[1]) == FAILURE)
+        return (print_error("Unable to get light position"));
+    if (!is_double_compatible(data[2]))
+        return (print_error("Unable to get light brightness"));
+    if (is_color_compatible(data[3]) == FAILURE)
+        return (print_error("Unable to get light color"));
+    return (SUCCESS);
+}
+
 int	process_light(char **data, t_element *element)
 {
     t_light light;
 
-    if (split_length(data) != 4)
-        return (print_error("Invalid values for light"));
+    if (check_light_values(data) == FAILURE)
+        return (FAILURE);
     light.position = extract_vector(data[1]);
     light.brightness = string_to_double(data[2]);
     light.color = extract_color(data[3]);
