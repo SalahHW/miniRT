87cc328a6a34f3d56fe1a355f4086ceb21542d8c
Salah <salah.bouheniche@gmail.com>
2024-02-28 04:35:10 +0100

    Refactor camera processing function

--- source/element/camera.c
+++ source/element/camera.c
@@ -6,32 +6,37 @@
 /*   By: sbouheni <sbouheni@student.42mulhouse.f    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/21 08:30:32 by sbouheni          #+#    #+#             */
-/*   Updated: 2024/02/09 19:39:48 by sbouheni         ###   ########.fr       */
+/*   Updated: 2024/02/27 17:04:26 by sbouheni         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../include/miniRT.h"
 
+static int check_camera_values(char **data)
+{
+    if (split_length(data) != 4)
+        return (print_error("Invalid syntax for camera"));
+    if (is_vector_compatible(data[1]) == FAILURE)
+        return (print_error("Unable to get camera position"));
+    if (is_vector_compatible(data[2]) == FAILURE)
+        return (print_error("Unable to get camera orientation"));
+    if (!is_double_compatible(data[3]))
+        return (print_error("Unable to get camera fov"));
+    return (SUCCESS);
+}
+
 int	process_camera(char **data, t_element *element)
 {
     t_camera camera;
 
-    if (split_length(data) != 4)
-        return (print_error("Invalid values for camera"));
-    if (!is_double_compatible(data[3]))
-        return (print_error("Unable to get camera fov"));
-    camera.fov = string_to_double(data[3]);
+    if (check_camera_values(data) == FAILURE)
+        return (FAILURE);
     camera.position = extract_vector(data[1]);
-    if (!camera.position)
-        return (print_error("Unable to get camera position"));
     camera.orientation = extract_vector(data[2]);
-    if (!camera.orientation)
-    {
-        clear_vector(camera.position);
-        return (print_error("Unable to get camera orientation"));
-    }
+    camera.fov = string_to_double(data[3]);
     element->u_element.camera = camera;
     element->initialized = 1;
+    return (SUCCESS);
 }
 
 void clear_camera(t_element *element)
