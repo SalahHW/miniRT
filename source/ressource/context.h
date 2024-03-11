64fe4d12888f6e39833ca4f0d115d7e51bf69844
Salah <salah.bouheniche@gmail.com>
2024-03-11 19:13:23 +0100

    Add init_base_cam function and base_cam field to context struct

--- source/ressource/context.h
+++ source/ressource/context.h
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   context.h                                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: sbouheni <sbouheni@student.42mulhouse.f    +#+  +:+       +#+        */
+/*   By: sbouheni <sbouheni@student.42mulhouse.fr>  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/27 07:04:19 by sbouheni          #+#    #+#             */
-/*   Updated: 2024/02/29 15:09:51 by sbouheni         ###   ########.fr       */
+/*   Updated: 2024/03/11 13:33:16 by sbouheni         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,9 +22,12 @@ typedef struct s_context
 	t_rt_file		*rt_file;
 	t_element_list	*element_list;
 	t_ambient_light *ambient_light;
-	t_camera 		*camera;
+	t_camera 		*camera; 
 	t_light			*light;
 	t_mlx_session	*mlx_session;
+	t_dir_pixel		**dir_pixel;
+	t_base			*base_cam;
+	double			dist_cam;
 }					t_context;
 
 t_context			*init_context(char *file_path);
