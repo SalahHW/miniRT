96fd07215303dd852bce437ad87afe32ec187196
Salah <salah.bouheniche@gmail.com>
2024-03-22 19:18:49 +0100

    Fix norm

--- source/parser/size_check.c
+++ source/parser/size_check.c
@@ -6,15 +6,15 @@
 /*   By: sbouheni <sbouheni@student.42mulhouse.fr>  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/22 19:11:56 by sbouheni          #+#    #+#             */
-/*   Updated: 2024/03/22 19:12:10 by sbouheni         ###   ########.fr       */
+/*   Updated: 2024/03/22 19:18:31 by sbouheni         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../include/miniRT.h"
 
-int check_size(double size)
+int	check_size(double size)
 {
-    if (size < 0 || size > 999)
-        return (FAILURE);
-    return (SUCCESS);
+	if (size < 0 || size > 999)
+		return (FAILURE);
+	return (SUCCESS);
 }
